369dbf115d895450c3111399edb9b63c9d070b85
angelmlc242003 <angelmlc200324@gmail.com>
2026-02-26 23:48:22 -0300

    refactor: remove particles background and simplify project structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
 import Footer from "./components/Footer";
-import ParticlesBackground from "./components/ParticlesBackground";
 import ContactPage from "./components/ContactPage";
 import Services from "./components/Services";
 import "./App.css";
@@ -11,7 +10,6 @@ import Hero from "./components/Hero";
 function App() {
   return (
     <>
-      <ParticlesBackground />
       <div className="App-content">
         <Navbar />
         <Hero />
